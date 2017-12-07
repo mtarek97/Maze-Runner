d6d1eeadb7ad8cad3d705874e125f3040dbcd957
abdelrahman882 <abdelrahmanahmed882@gmail.com>
2017-12-07 21:51:56 +0200

    new branch mapCell

--- src/mazeRunner/model/mapElement/MapCell.java
+++ src/mazeRunner/model/mapElement/MapCell.java
@@ -1,10 +1,7 @@
 package mazeRunner.model.mapElement;
 
 public interface MapCell {
-
-	//common properties and operations
-	
 	public boolean isObstacle();
-	
 	public boolean isDestroyable();
+	
 }
