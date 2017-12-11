69bbb081d6923e4291f700ab9c722e00eb17b8d5
Mustafa <mostafa.labib65@gmail.com>
2017-12-11 13:43:31 +0200

    view & controller desgin

--- src/mazeRunner/controller/MainController.java
+++ src/mazeRunner/controller/MainController.java
@@ -1,5 +1,24 @@
 package mazeRunner.controller;
 
+import mazeRunner.model.GameSetting;
+import mazeRunner.model.levels.MapSize;
+import mazeRunner.model.mapBuilder.MazeGenerator;
+import mazeRunner.view.GUI;
+import mazeRunner.view.mapElemenentsView.Recources;
+
 public class MainController {
+    private static MainController controller = new MainController();
+    private GUI gui = new GUI();
+    private GameSetting setting = new GameSetting();
+    private Recources recources = Recources.getRecources();
+    private MainController(){}
+    //TODO changing maze generator with map generator
+    private MapSize map = new MapSize(20,20);
+    private MazeGenerator generator = new MazeGenerator(map);
+    private boolean[][] maze =  generator.generateMaze();
+
+    public static MainController getController(){
+        return controller;
+    }
 
 }
