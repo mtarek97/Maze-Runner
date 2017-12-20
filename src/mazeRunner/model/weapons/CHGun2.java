aa284cab1addc466dece498066983960a4044c75
abdelrahman882 <abdelrahmanahmed882@gmail.com>
2017-12-21 01:24:52 +0200

    adding 3 guns

---
+++ src/mazeRunner/model/weapons/CHGun2.java
@@ -0,0 +1,46 @@
+package mazeRunner.model.weapons;
+
+public class CHGun2 implements Weapon{
+	private int ammo;
+	private int bulletsCount;
+	private int oneBulletDamage;
+	private String imageLink = "";
+
+	public CHGun2(int ammo, int oneBulletDamage){
+		this.ammo = ammo;
+		this.bulletsCount = ammo;
+		this.oneBulletDamage = oneBulletDamage;
+	}
+
+
+	public int getAmmo() {
+		return ammo;
+	}
+
+	public int getBulletsCount() {
+		return bulletsCount;
+	}
+
+	@Override
+	public void shoot() {
+		bulletsCount--;
+	}
+
+
+	@Override
+	public String getImageLink() {
+		return imageLink;
+	}
+	
+	@Override
+	public int getOneBulletDamage() {
+		return oneBulletDamage;
+	}
+
+
+	@Override
+	public void setFullAmmo() {
+		bulletsCount = ammo;
+		
+	}	
+}
