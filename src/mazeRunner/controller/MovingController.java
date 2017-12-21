eeced8c853fdfafbe66ab2e08427edcbaeaf2b46
abdelrahman882 <abdelrahmanahmed882@gmail.com>
2017-12-21 02:28:30 +0200

    layer2

--- src/mazeRunner/controller/MovingController.java
+++ src/mazeRunner/controller/MovingController.java
@@ -246,7 +246,7 @@ public class MovingController{
         if((row >= 0&& column >= 0&& row < SolidWallAndWaysArray.length&& column < SolidWallAndWaysArray[0].length)) {
 
             if (SolidWallAndWaysArray[row][column].isWay()&&!mapCellsArray[row][column].isObstacle()&&!mapCellsArray[row][column].isNonObstacle()) {
-                return true;
+                return true; 
             }
 
         }
